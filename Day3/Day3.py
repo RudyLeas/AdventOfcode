5f2e9fdc70d7c4d66441cf31bb3e9322f59a836b
rudyleas <jakub1.lis@student.uj.edu.pl>
2022-12-12 23:32:47 +0100

    Day part I solved

---
+++ Day3/Day3.py
@@ -0,0 +1,32 @@
+with open(r"C:\Users\lisku\Desktop\AdventOfCode\Day3\day3.txt","r" ) as f:
+    compartments = f.readlines()
+comp1= []
+comp2= []
+for count,i in enumerate (compartments):
+    comp1.append(i[:len(i)//2])
+    comp2.append(i[len(i)//2:])
+
+# print(comp1)
+# print(comp2)
+print(len(comp1))
+print(len(comp2))
+
+def priority(a):
+    val=ord(a)
+    if(val>96):
+        return(val-96)
+    if(val<91):
+        return(val-38)
+letters=[]
+for i in range(0,len(comp1)):
+    for j in comp1[i]:
+        if j in comp2[i]:
+            letters.append(j)
+            break
+sum=0
+for i in range(len(letters)):
+    sum+=priority(letters[i])
+print(sum)
+
+
+
